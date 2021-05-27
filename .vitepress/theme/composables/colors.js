9f7d3125455981e915c2d0b12026cbafe6a2c71d
davay42 <fond@fondfrukt.ru>
2021-05-27 15:15:00 +0300

    color mixing!

--- .vitepress/theme/composables/colors.js
+++ .vitepress/theme/composables/colors.js
@@ -1,11 +1,15 @@
 import { colord, extend } from 'colord'
 import lchPlugin from 'colord/plugins/lch'
 // https://www.npmjs.com/package/colord
+import mixPlugin from 'colord/plugins/mix'
 
+extend([mixPlugin])
 extend([lchPlugin])
 
-export function lchToHsl(n, total, a, s) {
-  let lch = `lch(70% ${s ? s : '40'} ${n * (360 / total)} / ${a ? a : '0.5'})`
+export const clrd = colord
+
+export function lchToHsl(n = 0, total = 12, a = 0.5, s = 40) {
+  let lch = `lch(70% ${s} ${n * (360 / total)} / ${a})`
   let hsl = colord(lch).toHslString()
   return hsl
 }
@@ -23,3 +27,13 @@ export function levelColor(
   }
   return `hsla(${i * (360 / n)}, ${s}, ${l}, ${a})`
 }
+
+const mixedColor = computed(() => {
+  let mixer = clrd(lchToHsl(props.tonic, 12, 1))
+  props.chroma.split('').forEach((bit, i) => {
+    if (isInChord(i)) {
+      mixer = mixer.mix(lchToHsl(i, 12, 1), 0.5)
+    }
+  })
+  return mixer.toHslString()
+})

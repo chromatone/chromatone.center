a84a968be178573207f8de12bf3d032b6f473d79
davay42 <fond@fondfrukt.ru>
2021-06-09 11:55:20 +0300

    bump

--- .vitepress/use/theory.js
+++ .vitepress/use/theory.js
@@ -5,7 +5,7 @@ ChordType.add(['1P', '2M'], ['2M'], 'major second')
 ChordType.add(['1P', '3m'], ['3m'], 'minor third')
 ChordType.add(['1P', '3M'], ['3M'], 'major third')
 ChordType.add(['1P', '4P'], ['4P'], 'perfect fourth')
-ChordType.add(['1P', '5d'], ['5d'], 'tritone')
+ChordType.add(['1P', '5d'], ['TT'], 'tritone')
 
 ChordType.add(['1P', '5P'], ['5P'], 'perfect fifth')
 ChordType.add(['1P', '6m'], ['6m'], 'minor sixth')

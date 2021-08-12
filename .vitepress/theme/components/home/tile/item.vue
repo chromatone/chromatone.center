d8971a77f5fb3d6fc378f60261f310a854035919
davay42 <fond@fondfrukt.ru>
2021-08-12 20:06:00 +0300

    v-bind and hover!

--- .vitepress/theme/components/home/tile/item.vue
+++ .vitepress/theme/components/home/tile/item.vue
@@ -1,5 +1,5 @@
 <template lang="pug">
-a.tile(:href="item.link", :style="{ color: color, boxShadow: `0 0 0 2px ${color}, 0 8px 0 2px ${color}` }")
+a.tile(:href="item.link", :style="{ color: color }")
   // img.cover(:src="'/media/' + item.data.cover")
   .flex.flex-col.p-4
     .text-3xl.mb-4.font-bold {{ item.title }}
@@ -14,12 +14,18 @@ const props = defineProps({
   total: Number,
 });
 const color = computed(() => lchToHsl(props.i, props.total));
+const shadow = computed(() => `0 0 0 2px ${color.value}, 0 6px 0 2px ${color.value}`);
+const hoverShadow = computed(() => `0 0 0 2px ${color.value}, 0 12px 0 4px ${color.value}`);
 </script>
 
 <style scoped>
 .tile {
   @apply bg-light-200 dark:bg-dark-300 m-4 flex flex-col transition no-underline;
+  box-shadow: v-bind(shadow);
   flex: 1 1 200px;
+  &:hover {
+    box-shadow: v-bind(hoverShadow);
+  }
 }
 
 .cover {

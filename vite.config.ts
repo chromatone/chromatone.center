b84b9bc98a74c63a0ca195b0dc4a2ffa3b9cd187
davay42 <fond@fondfrukt.ru>
2021-08-31 09:34:10 +0300

    unplugin components

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,5 @@
 import { defineConfig } from 'vite'
-import Components from 'vite-plugin-components'
+import Components from 'unplugin-vue-components/vite'
 import Icons from 'unplugin-icons/vite'
 import IconsResolver from 'unplugin-icons/resolver'
 import WindiCSS from 'vite-plugin-windicss'
@@ -68,8 +68,9 @@ export default defineConfig({
       extensions: ['vue', 'ts'],
       directoryAsNamespace: true,
       globalNamespaces: ['global'],
-      customLoaderMatcher: id => id.endsWith('.md'),
-      customComponentResolvers: [
+      include: [/\.vue$/, /\.vue\?vue/, /\.md$/],
+      exclude: [/node_modules/, /\.git/],
+      resolvers: [
         IconsResolver({
           componentPrefix: '',
         }),

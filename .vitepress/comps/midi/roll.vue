690e516c3f19f5f9fcaf481fd3f68a8ebcc8e933
davay42 <fond@fondfrukt.ru>
2021-09-22 00:00:07 +0300

    vertical!

--- .vitepress/comps/midi/roll.vue
+++ .vitepress/comps/midi/roll.vue
@@ -45,10 +45,10 @@ watch(() => midi.playing, playing => {
 let canvas, ctx, tempCanvas, tempCtx
 const state = reactive({
   initiated: false,
-  width: 400,
+  width: 508,
   height: 256,
-  speed: 1,
-  direction: 1,
+  speed: useStorage('midi-roll-speed', 1),
+  direction: useStorage('midi-roll-direction', 1),
 })
 
 onMounted(() => {
@@ -69,7 +69,7 @@ function initiate() {
 }
 
 function onCanvasDraw() {
-  if (state.vertical == 1) {
+  if (state.direction == 1) {
     drawVertical()
   } else {
     drawHorizontal()
@@ -79,17 +79,17 @@ function onCanvasDraw() {
 function drawVertical() {
   tempCtx.drawImage(canvas, 0, 0, state.width, state.height)
   ctx.fillStyle = '#333'
-  ctx.fillRect(state.width - state.speed, 0, state.speed, state.height)
-  ctx.fillStyle = '#4009'
-  for (let i = 0; i < 10; i++) {
-    let num = 234 - i * 24
-    ctx.fillRect(state.width - state.speed, state.height - num, state.speed, 0.5)
+  ctx.fillRect(0, 0, state.width, state.speed)
+  for (let i = 0; i < 127; i++) {
+    let num = i * 4
+    ctx.fillStyle = colorIt((i + 3) % 12, 1, 0.1)
+    ctx.fillRect(num + 2, 0, 1, state.speed)
   }
   score.notes.forEach(note => {
     ctx.fillStyle = colorIt((note + 3) % 12, 1)
-    ctx.fillRect(state.width - state.speed, state.height - note * 2, state.speed, 2)
+    ctx.fillRect(note * 4, 0, 4, state.speed * 2)
   })
-  ctx.translate(-state.speed, 0)
+  ctx.translate(0, state.speed)
   ctx.drawImage(tempCanvas, 0, 0, state.width, state.height)
   ctx.setTransform(1, 0, 0, 1, 0, 0);
 }
@@ -98,10 +98,10 @@ function drawHorizontal() {
   tempCtx.drawImage(canvas, 0, 0, state.width, state.height)
   ctx.fillStyle = '#333'
   ctx.fillRect(state.width - state.speed, 0, state.speed, state.height)
-  ctx.fillStyle = '#4009'
-  for (let i = 0; i < 10; i++) {
-    let num = 234 - i * 24
-    ctx.fillRect(state.width - state.speed, state.height - num, state.speed, 0.5)
+  for (let i = 0; i < 127; i++) {
+    let num = 127 - i
+    ctx.fillStyle = colorIt((num + 3) % 12, 1, 0.1)
+    ctx.fillRect(state.width - state.speed, state.height - num * 2, state.speed, 0.5)
   }
   score.notes.forEach(note => {
     ctx.fillStyle = colorIt((note + 3) % 12, 1)
@@ -110,10 +110,17 @@ function drawHorizontal() {
   ctx.translate(-state.speed, 0)
   ctx.drawImage(tempCanvas, 0, 0, state.width, state.height)
   ctx.setTransform(1, 0, 0, 1, 0, 0);
+
+  // score.notes.forEach(note => {
+  //   ctx.fillStyle = colorIt((note + 3) % 12, 1)
+  //   ctx.beginPath();
+  //   ctx.arc(state.width - 4, state.height - note * 2, 1, 0, 2 * Math.PI, false);
+  //   ctx.fill();
+  // })
 }
 
-function colorIt(pitch, value) {
-  return `hsl(${pitch * 30}, ${value * 100}%, ${value * 80}%)`
+function colorIt(pitch = 0, value = 1, opacity = 1) {
+  return `hsla(${pitch * 30}, ${value * 100}%, ${value * 60}%, ${opacity})`
 }
 
 </script>

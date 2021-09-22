9558d149a1c998949ad129281c3ebe932821c61b
davay42 <fond@fondfrukt.ru>
2021-09-22 15:11:51 +0300

    roll

--- .vitepress/comps/midi/roll.vue
+++ .vitepress/comps/midi/roll.vue
@@ -1,10 +1,10 @@
 <template lang="pug">
-.flex.flex-col.items-center
+.flex.flex-col.items-center.w-full
   midi-panel
   .flex(v-if="!state.initiated")
     start-button( @click="initiate()") Start
   .flex.flex-col
-    canvas#spectrogram.m-4.h-30em.max-w-full.rounded-md(
+    canvas#spectrogram.m-4.h-80vh.w-full.rounded-md(
       :width="state.width"
       :height="state.height"  
     )
@@ -24,7 +24,7 @@ const score = reactive({
       for (let n in midi.channels[ch].notes) {
         let note = midi.channels[ch].notes[n]
         if (note.velocity > 0) {
-          activeNotes.push(note._number)
+          activeNotes.push(note)
         }
       }
     }
@@ -46,7 +46,7 @@ let canvas, ctx, tempCanvas, tempCtx
 const state = reactive({
   initiated: false,
   width: 508,
-  height: 256,
+  height: 508,
   speed: useStorage('midi-roll-speed', 1),
   direction: useStorage('midi-roll-direction', 1),
 })
@@ -78,34 +78,35 @@ function onCanvasDraw() {
 
 function drawVertical() {
   tempCtx.drawImage(canvas, 0, 0, state.width, state.height)
-  ctx.fillStyle = '#333'
+  ctx.fillStyle = '#33333399'
   ctx.fillRect(0, 0, state.width, state.speed)
   for (let i = 0; i < 127; i++) {
     let num = i * 4
     ctx.fillStyle = colorIt((i + 3) % 12, 1, 0.1)
-    ctx.fillRect(num + 2, 0, 1, state.speed)
+    ctx.fillRect(num, 0, 1, state.speed)
   }
-  score.notes.forEach(note => {
-    ctx.fillStyle = colorIt((note + 3) % 12, 1)
-    ctx.fillRect(note * 4, 0, 4, state.speed * 2)
-  })
   ctx.translate(0, state.speed)
   ctx.drawImage(tempCanvas, 0, 0, state.width, state.height)
+  score.notes.forEach(note => {
+    const size = 16 - note.channel
+    ctx.fillStyle = colorIt((note.number + 3) % 12, 1)
+    ctx.fillRect(note.number * 4 - size / 2, 0, size, state.speed * 2)
+  })
   ctx.setTransform(1, 0, 0, 1, 0, 0);
 }
 
 function drawHorizontal() {
   tempCtx.drawImage(canvas, 0, 0, state.width, state.height)
-  ctx.fillStyle = '#333'
+  ctx.fillStyle = '#33333399'
   ctx.fillRect(state.width - state.speed, 0, state.speed, state.height)
   for (let i = 0; i < 127; i++) {
-    let num = 127 - i
-    ctx.fillStyle = colorIt((num + 3) % 12, 1, 0.1)
-    ctx.fillRect(state.width - state.speed, state.height - num * 2, state.speed, 0.5)
+    let num = (127 - i) * (state.height / 127)
+    ctx.fillStyle = colorIt((num + 3) % 12, 0.5, 0.1)
+    ctx.fillRect(0, state.height - num, state.width, 0.5)
   }
   score.notes.forEach(note => {
-    ctx.fillStyle = colorIt((note + 3) % 12, 1)
-    ctx.fillRect(state.width - state.speed, state.height - note * 2, state.speed, 2)
+    ctx.fillStyle = colorIt((note.number + 3) % 12, 1)
+    ctx.fillRect(state.width - state.speed, state.height - note.number * state.height / 127, state.speed, 12 - note.channel / 2)
   })
   ctx.translate(-state.speed, 0)
   ctx.drawImage(tempCanvas, 0, 0, state.width, state.height)
---
+++ music/chords/triads.md
@@ -0,0 +1,36 @@
+---
+title: Triads
+subtitle: Chords consisting of three notes
+tags: chords
+date: 2021-09-22
+---
+
+## Major chord
+
+<abc-render abc="[A4^ce] A^ce" />
+
+<chroma-circle :chroma="'100010010000'" type="M" />
+
+<chroma-row :chroma="'100010010000'" />
+
+A major triad can also be described by its intervals: the interval between the bottom and middle notes is a major third and the interval between the middle and top notes is a minor third.
+
+## Minor chord
+
+<abc-render abc="[A4ce] Ace" />
+
+<chroma-circle :chroma="'100100010000'" type="m" />
+
+<chroma-row :chroma="'100100010000'" />
+
+## Augmented chord
+
+## Diminished chord
+
+## Suspended 2 chord
+
+## Suspended 4 chord
+
+## Major b5 chord
+
+## Minor #5 chord

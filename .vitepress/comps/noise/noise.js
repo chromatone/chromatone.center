d9b764b4839c1597fba5aed2caa59c8b25ae5d64
davay <fond@fondfrukt.ru>
2022-01-17 19:07:12 +0300

    to be corrected

--- .vitepress/comps/noise/noise.js
+++ .vitepress/comps/noise/noise.js
@@ -7,13 +7,13 @@ import {
   AutoFilter,
   AutoPanner,
   BitCrusher,
-} from 'tone'
-import { useStorage, useRafFn, onKeyStroke } from '@vueuse/core'
+} from "tone";
+import { useStorage, useRafFn, onKeyStroke } from "@vueuse/core";
 
 export function useNoise() {
-  const options = useStorage('noise-options', {
+  const options = useStorage("noise-options", {
     noise: {
-      type: 'pink',
+      type: "pink",
     },
     envelope: {
       attack: 0.1,
@@ -22,9 +22,9 @@ export function useNoise() {
       release: 1,
     },
     volume: 1,
-  })
+  });
 
-  const filterOptions = useStorage('filter-options', {
+  const filterOptions = useStorage("filter-options", {
     on: false,
     play: false,
     volume: 1,
@@ -33,128 +33,130 @@ export function useNoise() {
     frequency: 1,
     octaves: 2,
     wet: 1,
-    type: 'sine',
+    type: "sine",
     filter: {
       Q: 1,
-      type: 'lowpass',
+      type: "lowpass",
     },
-  })
+  });
 
-  const pannerOptions = useStorage('panner-options', {
+  const pannerOptions = useStorage("panner-options", {
     on: false,
     play: false,
     wet: 1,
     frequency: 1,
     depth: 1,
     volume: 1,
-  })
+  });
 
-  const crusherOptions = useStorage('bit-options', {
+  const crusherOptions = useStorage("bit-options", {
     on: false,
     bits: 16,
     wet: 1,
     volume: 1,
-  })
+  });
 
-  const active = ref(false)
-  const fftData = ref([])
-  const fftFreq = ref([])
-  const types = { brown: 'brown', pink: 'pink', white: 'white' }
-  const filterTypes = { lowpass: 'LP', highpass: 'HP', bandpass: 'BP' }
+  const active = ref(false);
+  const fftData = ref([]);
+  const fftFreq = ref([]);
+  const types = { brown: "brown", pink: "pink", white: "white" };
+  const filterTypes = { lowpass: "LP", highpass: "HP", bandpass: "BP" };
   const filterLFOTypes = {
-    sine: 'SIN',
-    triangle: 'TRI',
-    square: 'SQR',
-    sawtooth: 'SAW',
-  }
-  const fft = new FFT({ size: 512, smoothing: 0.2 }).toDestination()
+    sine: "SIN",
+    triangle: "TRI",
+    square: "SQR",
+    sawtooth: "SAW",
+  };
+  const fft = new FFT({ size: 512, smoothing: 0.2 }).toDestination();
 
   for (let j = 0; j < 32; j++) {
-    fftFreq.value[j] = fft.getFrequencyOfIndex(j)
+    fftFreq.value[j] = fft.getFrequencyOfIndex(j);
   }
-  const gain = new Gain(options.value.volume).connect(fft)
-  const filterGain = new Gain(filterOptions.value.volume).connect(fft)
-  const pannerGain = new Gain(pannerOptions.value.volume).connect(fft)
-  const crusherGain = new Gain(crusherOptions.value.volume).connect(fft)
-  const panner = new AutoPanner(pannerOptions.value).connect(pannerGain)
+  const gain = new Gain(options.value.volume).connect(fft);
+  const filterGain = new Gain(filterOptions.value.volume).connect(fft);
+  const pannerGain = new Gain(pannerOptions.value.volume).connect(fft);
+  const crusherGain = new Gain(crusherOptions.value.volume).connect(fft);
+  const panner = new AutoPanner(pannerOptions.value).connect(pannerGain);
   const crusher = new BitCrusher(crusherOptions.value)
     .connect(crusherGain)
-    .connect(panner)
+    .connect(panner);
 
   const filter = new AutoFilter(filterOptions.value)
     .connect(filterGain)
-    .connect(crusher)
-  const synth = new NoiseSynth(options.value).connect(gain).connect(filter)
+    .connect(crusher);
+  const synth = new NoiseSynth(options.value).connect(gain).connect(filter);
 
   const { pause, resume } = useRafFn(() => {
-    let arr = fft.getValue()
+    let arr = fft.getValue();
     for (let j = 0; j < 32; j++) {
-      fftData.value[j] = dbToGain(arr[j]) * 10
+      fftData.value[j] = dbToGain(arr[j]) * 10;
     }
-  })
+  });
 
   onKeyStroke(
-    ' ',
+    " ",
     (e) => {
-      e.preventDefault()
-      active.value = true
+      e.preventDefault();
+      active.value = true;
     },
-    { eventName: 'keydown' },
-  )
+    { eventName: "keydown" }
+  );
 
   onKeyStroke(
-    ' ',
+    " ",
     (e) => {
-      active.value = false
+      active.value = false;
     },
-    { eventName: 'keyup' },
-  )
+    { eventName: "keyup" }
+  );
 
   watch(active, (act) => {
     if (act) {
-      synth.triggerAttack()
+      synth.triggerAttack();
     } else {
-      synth.triggerRelease()
+      synth.triggerRelease();
     }
-  })
+  });
 
   watch(options.value, () => {
-    synth.set(options.value)
-  })
+    synth.set(options.value);
+  });
 
   watch(
     () => options.value.volume,
     (vol) => {
-      gain.gain.rampTo(vol, 1)
-    },
-  )
+      gain.gain.rampTo(vol, 1);
+    }
+  );
 
   onBeforeUnmount(() => {
-    synth.triggerRelease()
-  })
+    synth.triggerRelease();
+  });
 
   watch(filterOptions.value, (opt) => {
-    opt.play ? filter.start() : filter.stop()
-    opt.on
-      ? filterGain.gain.rampTo(filterOptions.value.volume, 0.2)
-      : filterGain.gain.rampTo(0, 0.2)
-    filter.set(opt)
-  })
+    opt.play ? filter.start() : filter.stop();
+    if (opt.on) {
+      filterGain.gain.rampTo(filterOptions.value.volume, 0.2);
+    } else {
+      filterGain.gain.rampTo(0, 0.2);
+    }
+    filter.set(opt);
+  });
 
   watch(pannerOptions.value, (opt) => {
-    opt.play ? panner.start() : panner.stop()
+    opt.play ? panner.start() : panner.stop();
     opt.on
       ? pannerGain.gain.rampTo(pannerOptions.value.volume, 0.2)
-      : pannerGain.gain.rampTo(0, 0.2)
-    panner.set(opt)
-  })
+      : pannerGain.gain.rampTo(0, 0.2);
+    panner.set(opt);
+  });
 
   watch(crusherOptions.value, (opt) => {
     opt.on
       ? crusherGain.gain.rampTo(crusherOptions.value.volume, 0.2)
-      : crusherGain.gain.rampTo(0, 0.2)
-    crusher.set(opt)
-  })
+      : crusherGain.gain.rampTo(0, 0.2);
+    crusher.set(opt);
+  });
 
   return {
     options,
@@ -167,5 +169,5 @@ export function useNoise() {
     types,
     filterTypes,
     filterLFOTypes,
-  }
+  };
 }

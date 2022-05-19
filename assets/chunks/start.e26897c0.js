f8875fc7444b0220b72577eb1b88ca558582530f
davay <fond@fondfrukt.ru>
2022-05-19 16:47:14 +0300

    deploy

--- assets/chunks/start.e26897c0.js
+++
@@ -1,1 +0,0 @@
-import{o as t,c as _,R as o,k as s,_ as a}from"../app.d144df73.js";const n={class:"button"},r=s("Start");function c(e,u){return t(),_("button",n,[o(e.$slots,"default",{},()=>[r],!0)])}const d={};var p=a(d,[["render",c],["__scopeId","data-v-ba393ede"]]);export{p as _};

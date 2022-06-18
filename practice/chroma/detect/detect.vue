e7a9c67fa6f887cec28e2d0b07cad9166dfccc1d
davay <fond@fondfrukt.ru>
2022-06-18 21:32:13 +0300

    feat: blur and radius control on chroma detect

--- practice/chroma/detect/detect.vue
+++ practice/chroma/detect/detect.vue
@@ -4,13 +4,16 @@ import { notes } from '@use/theory'
 import { useTuner } from '@use/tuner.js'
 const { init, tuner, chain } = useTuner();
 
+const blur = ref(0)
+const filter = computed(() => `blur(${blur.value}px)`)
+const radius = useClamp(2, 1, 5)
 </script>
 
 <template lang="pug">
-.fullscreen-container.rounded-4xl#screen
+.fullscreen-container.rounded-4xl
   control-start.absolute(v-if="!tuner.initiated" @click="init()") Start
   full-screen.absolute.bottom-6.right-6.z-30
-  svg.max-h-3xl.w-full(
+  svg.max-h-3xl.w-full#screen(
   version="1.1",
   baseProfile="full",
   viewBox="0 0 100 100",
@@ -43,8 +46,8 @@ const { init, tuner, chain } = useTuner();
       circle.note(
         style="transition: all 300ms ease-in-out;transform-box: fill-box; transform-origin: center center;"
 
-        :r="2",
-        :style="{ transform: `scale(${0.5 + 5 * amount})` }"
+        :r="radius",
+        :style="{ transform: `scale(${0.5 + 10 * amount})` }"
         :fill="pitchColor(i, 3, amount)",
       )
       text(
@@ -55,10 +58,26 @@ const { init, tuner, chain } = useTuner();
         text-anchor="middle",
         dominant-baseline="middle"
       ) {{ notes[i] }} 
+  .flex
+    control-rotary(
+      param="blur"
+      :min="0"
+      :max="10"
+      :step="0.1"
+      v-model="blur"
+      )
+    control-rotary(
+      param="radius"
+      :min="1"
+      :max="5"
+      :step="0.1"
+      v-model="radius"
+      )
 </template>
 
 <style lang="postcss" scoped>
 .note {
+  filter: v-bind(filter);
   /* @apply mix-blend-screen; */
 }
 </style>

9c34c450992c31e8123238e9c350d098d9a01ec8
davay <fond@fondfrukt.ru>
2022-07-21 11:41:21 +0300

    brain research

---
+++ practice/experiments/brain/brain.vue
@@ -0,0 +1,32 @@
+<script setup>
+import { pitchColor } from '#use/calculations';
+import { midi } from '#use/midi';
+import { notes } from '#use/theory';
+import { colord } from 'colord';
+
+
+const width = ref(300)
+const height = ref(300)
+</script>
+
+<template lang='pug'>
+#screen.h-600px.w-full.transition(
+  :style="{ backgroundColor: pitchColor(midi?.note?.pitch, midi.note?.octA, 1, midi.note?.attack) }"
+)
+full-screen.absolute.right-4.top-4()
+
+
+.relative.hidden
+  svg-save(svg="brain")
+  svg#brain.w-full(
+    :width="width * 3 + 30"
+    :height="height * 4 + 40"
+    viewbox="-10 -10 1300 1000"
+    )
+    g(
+      v-for="(note, n) in notes" :key="note"
+      :transform="`translate(${(n % 3) * (width + 10)} ${Math.floor(n / 3) * (height + 10)})`"
+      )
+      rect(x="0" y="0" width="300" height="150" :fill="colord(`hsl(${n * 30},100%,50%)`).toHex()")
+      rect(x="0" y="150" width="300" height="150" :fill="colord(`hsl(${n * 30},100%,35%)`).toHex()")
+</template>
---
+++ practice/experiments/brain/index.md
@@ -0,0 +1,15 @@
+---
+title: Brain stimulation research
+subtitle: Full screen colors react to midi notes to explore the cross stimulation of the brain
+date: 2021-04-05
+---
+
+<script setup>
+import brain from './brain.vue'
+</script>
+
+<client-only>
+  <brain />
+</client-only>
+
+Press any key on your keyboard to hear a note and to see the corresponding color. Go fullscreen with the button at the top right.

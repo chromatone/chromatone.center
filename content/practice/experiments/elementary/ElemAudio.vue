f025125b3ebda6b35c37f4649448f2979068f72d
davay <davay@defucc.me>
2023-06-28 16:48:49 +0700

    ElSynth gettin shape

--- content/practice/experiments/elementary/ElemAudio.vue
+++ content/practice/experiments/elementary/ElemAudio.vue
@@ -15,74 +15,69 @@ function useElemSynth({
   numVoices = 8
 } = {}) {
 
-  const synth = {
-
-  }
-
-
-  const control = reactive({
-    gate: useClamp(1, 0, 1),
-    volume: useClamp(.2, 0, 1),
-    attack: useClamp(0.0001, 0, 6)
-  })
-
-  const voices = reactive(Array(numVoices).fill(true).map((_, i) => ({ gate: 0.0, freq: 440, key: `v${i}`, midi: 69 })))
-
-
-
-  function synthVoice(voice) {
-    return el.mul(0.2 * voice.gate, el.blepsaw(voice.freq))
-  }
-
-  const audio = computed(() => {
-    // CONTROLS
-    const ctrl = {}
-    for (let c in control) {
-      ctrl[c] = el.const({ key: `ctrl:${c}`, value: control[c] })
-    }
-
-    // OSC
-    let env = el.adsr(ctrl.attack, .5, .3, .2, ctrl.gate)
-    let beep = el.mul(env, el.blepsaw(440))
-
-    // Noise
-    let noise = el.mul(env, el.pinknoise())
-
-    // Effects
-    let summed = el.add(beep, el.mul(0.9, noise))
-    let filtered = el.lowpass(el.add(400, el.mul(3000, env)), 1.4, summed)
-    let delayed = el.mul(0.3, el.delay({ size: 44100 }, el.ms2samps(200), 0.2, filtered))
-    let wetdry1 = el.add(filtered, delayed);
-    // let reverbed = el.mul(0.3, el.convolve({ path: 'sample0' }, wetdry1))
-    // let wetdry2 = el.add(wetdry1, reverbed)
-
-    // INPUT
-    // let input = el.mul(el.in({ key: 'in' }), 1.0)
-    // let withInput = el.add(wetdry1, input)
-
-    // OUTPUT
-    let output = el.mul(wetdry1, ctrl.volume)
-
-    return output
-  })
-
-  let nextVoice = 0;
-  function startNote(num, velocity) {
-    do {
-      nextVoice++
-      if (nextVoice >= voices.length) {
-        nextVoice = 0
+  const es = reactive({
+    control: {
+      gate: useClamp(1, 0, 1),
+      volume: useClamp(.2, 0, 1),
+      attack: useClamp(0.001, 0, 6)
+    },
+    nextVoice: 0,
+    voices: Array(numVoices).fill(true).map((_, i) => ({ gate: 0.0, freq: 440, key: `v${i}`, midi: 69, vel: 0 })),
+    synthVoice(voice) {
+      const ctrl = {}
+      for (let c in es.control) {
+        ctrl[c] = el.const({ key: `ctrl:${c}`, value: es.control[c] })
       }
-    } while (voices[nextVoice].gate > 0)
 
-    voices[nextVoice].gate = velocity / 127
-    voices[nextVoice].freq = pitchFreq(num - 69)
-    voices[nextVoice].midi = num
-  }
+      // OSC
+      // let env = el.adsr(ctrl.attack, .5, .3, .3, voice.gate)
+      // let beep = el.mul(env, el.blepsquare(voice.freq))
 
-  function stopNote(num) {
-    voices.filter((v) => v.midi == num).map(v => v.gate = 0)
-  }
+      // // Noise
+      // let noise = el.mul(env, el.pinknoise())
+
+      // // Effects
+      // let summed = el.add(beep, el.mul(0.9, noise))
+      // let filtered = el.lowpass(el.add(400, el.mul(3000, env)), 1.4, summed)
+      // let delayed = el.mul(0.7, el.delay({ size: 44100 }, el.ms2samps(200), 0.2, filtered))
+      // let wetdry1 = el.add(filtered, delayed);
+      // let reverbed = el.mul(0.3, el.convolve({ path: 'sample0' }, wetdry1))
+      // let wetdry2 = el.add(wetdry1, reverbed)
+
+      // INPUT
+      // let input = el.mul(el.in({ key: 'in' }), 1.0)
+      // let withInput = el.add(wetdry1, input)
+
+      // OUTPUT
+      // let output = el.mul(wetdry1, ctrl.volume)
+      const env = el.adsr({ key: `${voice.key}:env` }, 0.001, .3, .4, 8, voice.gate)
+      const osc = el.blepsquare({ key: `${voice.key}:osc` }, voice.freq)
+      const envOsc = el.mul(env, osc)
+      const noise = el.mul(env, el.noise())
+
+      let summed = el.add(el.mul(0.5, envOsc), el.mul(.3, noise))
+
+      const output = el.mul(summed)
+
+      return output
+    },
+    startNote(num, velocity) {
+      do {
+        es.nextVoice++
+        if (es.nextVoice >= es.voices.length) {
+          es.nextVoice = 0
+        }
+      } while (es.voices[es.nextVoice].gate == 1)
+
+      es.voices[es.nextVoice].gate = 1
+      es.voices[es.nextVoice].vel = velocity / 127
+      es.voices[es.nextVoice].freq = pitchFreq(num - 69)
+      es.voices[es.nextVoice].midi = num
+    },
+    stopNote(num) {
+      es.voices.filter((v) => v.midi == num).map(v => v.gate = 0)
+    }
+  })
 
   onMounted(async () => {
 
@@ -91,21 +86,13 @@ function useElemSynth({
 
     core.on('load', async function () {
 
-      watch(voices, vcs => {
-        let vs = vcs.map(el => synthVoice(el))
-        let out = el.add(...vs)
-        core.render(out, out)
-      })
-
-      watch(audio, a => {
-        core.render(a, a)
-      }, { immediate: true })
-
-      setTimeout(() => {
-        control.gate = 0
-      }, 10)
-
     });
+
+    watch(es, () => {
+      let vs = es.voices.map(el => es.synthVoice(el))
+      let out = el.add(...vs)
+      core.render(out, out)
+    })
 
     let node = await core.initialize(ctx, {
       numberOfInputs: 0,
@@ -117,31 +104,21 @@ function useElemSynth({
 
   })
 
-  return {
-    voices,
-    control,
-    startNote,
-    stopNote
-  }
+  return es
 
 }
 
-
-const { control, voices, startNote, stopNote } = useElemSynth()
+const es = useElemSynth()
 
 const { midi } = useMidi()
 
 watch(() => midi.note, n => {
   if (n.velocity) {
-    startNote(n.number, n.velocity)
+    es.startNote(n.number, n.velocity)
   } else {
-    stopNote(n.number)
+    es.stopNote(n.number)
   }
 })
-
-onKeyDown('a', () => { control.gate = 1 })
-onKeyUp('a', () => { control.gate = 0 })
-
 
 </script>
 
@@ -150,20 +127,20 @@ onKeyUp('a', () => { control.gate = 0 })
   .text-2xl.p-2 Elementary
   control-rotary(
     :step="0.001"
-    v-model="control.volume"
+    v-model="es.control.volume"
     :max="1"
     param="Vol"
   )
-  pre {{ control }}
+  pre {{ es.control }}
 
   button.text-button(
-    @mousedown.passive="control.gate=1"
-    @mouseup.passive="control.gate=0"
-    @touchstart.prevent.stop="control.gate=1"
-    @touchend.prevent.stop="control.gate=0"
-    @mouseleave="control.gate=0"
+    @mousedown.passive="es.control.gate=1"
+    @mouseup.passive="es.control.gate=0"
+    @touchstart.prevent.stop="es.control.gate=1"
+    @touchend.prevent.stop="es.control.gate=0"
+    @mouseleave="es.control.gate=0"
     ) PRESS TO PLAY A SOUND
 
-  .text-xs(v-for="voice in voices" :key="voice") {{ voice }}
+  .text-xs(v-for="voice in es.voices" :key="voice") {{ voice }}
 
 </template>

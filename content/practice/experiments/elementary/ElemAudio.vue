cb4d714c4122f20508f13ced2d41cbe7d2cc3453
davay <davay@defucc.me>
2023-06-29 15:45:59 +0700

    freq in synth

--- content/practice/experiments/elementary/ElemAudio.vue
+++ content/practice/experiments/elementary/ElemAudio.vue
@@ -41,26 +41,30 @@ function useElemSynth({
     nextVoice: 0,
     overflow: 0,
     voices: Array(numVoices).fill(true).map((_, i) => ({ gate: 0.0, freq: 440, key: `v${i}`, midi: 69, vel: 0 })),
-    startNote(num, velocity) {
-      do {
-        es.nextVoice++
-        if (es.nextVoice >= es.voices.length) {
-          es.nextVoice = 0
-          es.overflow++
-        }
-        if (es.overflow > 3) break;
-      } while (es.voices[es.nextVoice].gate == 1)
-      es.overflow = 0
-      Object.assign(es.voices[es.nextVoice], {
-        gate: 1,
-        vel: velocity / 127,
-        freq: pitchFreq(num - 69),
-        midi: num
-      })
-
+    cycleNote(num, velocity) {
+      if (velocity) {
+        do {
+          es.nextVoice++
+          if (es.nextVoice >= es.voices.length) {
+            es.nextVoice = 0
+            es.overflow++
+          }
+          if (es.overflow > 3) break;
+        } while (es.voices[es.nextVoice].gate == 1)
+        es.overflow = 0
+        Object.assign(es.voices[es.nextVoice], {
+          gate: 1,
+          vel: velocity / 127,
+          freq: pitchFreq(num - 69),
+          midi: num
+        })
+      } else {
+        es.voices.filter((v) => v.midi == num).forEach(v => Object.assign(v, { gate: 0 })
+        )
+      }
     },
-    stopNote(num) {
-      es.voices.filter((v) => v.midi == num).forEach(v => Object.assign(v, { gate: 0 })
+    stopAll(num) {
+      es.voices.forEach(v => Object.assign(v, { gate: 0 })
       )
     },
     ctrl: computed(() => {
@@ -70,17 +74,9 @@ function useElemSynth({
       }
       return ctrl
     }),
-    cv: computed(() =>
-      es.voices.map((vc, i) => {
-        const v = {}
-        for (let p in vc) {
-          v[p] = el.const({ key: `${vc.key}:${p}`, value: vc[p] })
-        }
-        return v
-      })
-    ),
-    synth(voices = es.voices) {
-      return el.mul(
+
+    synth: (voices = es.voices) =>
+      el.mul(
         es.ctrl.volume,
         el.mul(
           el.sqrt(
@@ -89,37 +85,36 @@ function useElemSynth({
               value: 1 / voices.length
             })),
           el.add(...voices.map(
-            (_, i) => {
+            (vc, i) => {
               const v = {}
+              for (let p in vc) {
+                v[p] = el.const({ key: `${vc.key}:${p}`, value: vc[p] })
+              }
 
               v.env = el.mul(
-                es.cv[i].vel,
+                v.vel,
                 el.adsr(
                   es.ctrl.attack,
                   es.ctrl.decay,
                   es.ctrl.sustain,
                   es.ctrl.release,
-                  es.cv[i].gate)
-              )
+                  v.gate))
 
               v.envOsc = el.mul(
+                es.ctrl.osc1Gain,
                 v.env,
                 el.lowpass(
-                  el.mul(4, es.cv[i].freq),
+                  el.mul(4, v.freq),
                   1.1,
-                  el.blepsaw(es.cv[i].freq)))
+                  el.blepsaw(v.freq)))
 
               v.noise = el.mul(
+                es.ctrl.noiseGain,
                 v.env,
-                el.bandpass(es.cv[i].freq, 50, el.noise()))
+                el.bandpass(v.freq, 50, el.noise()))
 
-              v.summed = el.add(
-                el.mul(es.ctrl.osc1Gain, v.envOsc),
-                el.mul(es.ctrl.noiseGain, v.noise))
-
-              return v.summed
-            }))))
-    },
+              return el.add(v.envOsc, v.noise)
+            })))),
     async init() {
       const ctx = new AudioContext();
       const core = new WebRenderer({
@@ -131,7 +126,7 @@ function useElemSynth({
         watchEffect(() => {
           console.log('reacted')
 
-          const [outR, outL] = pingPong(es.synth())
+          const [outR, outL] = pingPong(es.synth(es.voices))
 
           core.render(outR, outL)
         })
@@ -159,11 +154,7 @@ const es = useElemSynth()
 const { midi } = useMidi()
 
 watch(() => midi.note, n => {
-  if (n.velocity) {
-    es.startNote(n.number, n.velocity)
-  } else {
-    es.stopNote(n.number)
-  }
+  es.cycleNote(n.number, n.velocity)
 })
 
 </script>
@@ -171,7 +162,7 @@ watch(() => midi.note, n => {
 <template lang="pug">
 .p-4.bg-light-200.dark-bg-dark-200.shadow.rounded.flex.flex-col.gap-4
   .text-2xl.p-2 Elementary 
-  .flex.is-group.p-2.gap-2
+  .flex.flex-wrap.is-group.p-2.gap-2
     control-rotary(
       v-for="param in es.params" :key="param.name"
       :step="param.step"
@@ -181,13 +172,15 @@ watch(() => midi.note, n => {
       :param="param.name"
       )
 
-  button.text-button.text-2xl(
-    @mousedown.passive="es.startNote(60, 120)"
-    @mouseup.passive="es.stopNote(60)"
-    @touchstart.prevent.stop="es.startNote(60, 120)"
-    @touchend.prevent.stop="es.stopNote(60)"
-    @mouseleave="es.stopNote(60)"
-    ) PRESS TO PLAY A NOTE
+  .flex.gap-4
+    button.text-button.text-2xl.flex-1(
+      @mousedown.passive="es.cycleNote(60, 120)"
+      @mouseup.passive="es.cycleNote(60)"
+      @touchstart.prevent.stop="es.cycleNote(60, 120)"
+      @touchend.prevent.stop="es.cycleNote(60)"
+      @mouseleave="es.cycleNote(60)"
+      ) PRESS TO PLAY A NOTE
+    button.text-button.text-2xl(@click="es.stopAll()") STOP ALL
 
   .flex.flex-col.gap-1.font-mono
     .text-xs.flex(

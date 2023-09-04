e168bff13f5f8ec8d2fb665e1e9d71ec8f91c4b0
davay <davay@defucc.me>
2023-09-04 20:48:31 +0700

    fixes

--- content/academy/events/EventDetails.vue
+++ content/academy/events/EventDetails.vue
@@ -20,25 +20,26 @@ const weekDay = useDateFormat(props.date, 'dddd')
 </script>
 
 <template lang='pug'>
-.flex.flex-col.gap-4.text-lg.mx-4.p-4.bg-light-100.dark-bg-dark-300.max-w-150.rounded-xl.shadow
+.flex.flex-col.gap-8.text-lg.mx-4.p-4.bg-light-100.dark-bg-dark-300.max-w-150.rounded-xl.shadow
   img(v-if="poster" style="margin:0;" :src=" `https://db.chromatone.center/assets/${poster}?format=webp`")
-  .flex.flex-col.gap-2 
-    .text-3xl.font-bold {{ title }}
-    .p-0 {{ description }}
-  .flex-1.flex.gap-2.items-center 
-    .i-la-clock
-    .p-0.font-bold {{ start_time?.slice(0,-3) }} 
-    .p-0 {{ weekDay }}
-    .p-0.opacity-40 {{ duration }}
-  .flex.flex-wrap.gap-2.items-center
-    .i-la-calendar
-    .p-0.font-bold(v-for="d in formattedDate.split(' ')" :key="d") {{ d }}
-    .p-0 {{ year }}
+  .flex.flex-col.gap-2
+    .flex.flex-col.gap-2 
+      .text-3xl.font-bold {{ title }}
+      .p-0 {{ description }}
+    .flex-1.flex.gap-2.items-center 
+      .i-la-clock
+      .p-0.font-bold {{ start_time?.slice(0,-3) }} 
+      .p-0 {{ weekDay }}
+      .p-0.opacity-40 {{ duration }}
+    .flex.flex-wrap.gap-2.items-center
+      .i-la-calendar
+      .p-0.font-bold(v-for="d in formattedDate.split(' ')" :key="d") {{ d }}
+      .p-0 {{ year }}
 
-  a.flex.items-center.gap-2.no-underline.font-normal(:href="`/academy/partners/${place?.slug}/`" v-if="place")
-    .i-la-map-marker
-    .p-0.font-bold {{ place?.title }},
-    .p-0 {{ place?.city }},
-    .p-0 {{ place?.country }}
+    a.flex.items-center.gap-2.no-underline.font-normal(:href="`/academy/partners/${place?.slug}/`" v-if="place")
+      .i-la-map-marker
+      .p-0.font-bold {{ place?.title }},
+      .p-0 {{ place?.city }},
+      .p-0 {{ place?.country }}
   img(v-if="cover" style="margin:0;" :src="cover")
 </template>

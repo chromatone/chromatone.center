6b36314138851b5dcfa653a503e418bc2129ea05
davay <davay@defucc.me>
2023-10-16 23:21:52 +0700

    deploy

--- assets/chunks/state.572f4e75.js
+++
@@ -1,1 +0,0 @@
-import{k as e,v as o}from"./framework.2f46aeb8.js";const t=e({colorize:o("colorize-notes",!1)});export{t as s};
---
+++ assets/chunks/state.b11ae059.js
@@ -0,0 +1,1 @@
+import{k as e,v as o}from"./framework.adad30f1.js";const t=e({colorize:o("colorize-notes",!1)});export{t as s};

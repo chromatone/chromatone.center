35a27ad477f3139c0d41a5ed4785127eb0175f03
davay42 <davay42@users.noreply.github.com>
2024-01-26 16:01:14 +0000

    deploy: 3dd6784db9f737d96f11143c6806883e5bf28333

---
+++ assets/chunks/state.yiZK73zQ.js
@@ -0,0 +1,1 @@
+import{b as e,u as o}from"./framework.NDjH5Ni5.js";const t=e({colorize:o("colorize-notes",!1)});export{t as s};
--- assets/chunks/virtual_pwa-register.YZAaYWTa.js
+++
@@ -1,7 +0,0 @@
-import{a4 as w}from"./framework.KnxPB1sj.js";function v(c={}){const{immediate:d=!1,onNeedRefresh:_,onOfflineReady:i,onRegistered:a,onRegisteredSW:s,onRegisterError:r}=c;let t,o;const l=async(n=!0)=>{await o};async function p(){if("serviceWorker"in navigator){const{Workbox:n}=await w(()=>import("./workbox-window.prod.es5.prqDwDSL.js"),__vite__mapDeps([]));t=new n("/sw.js",{scope:"/",type:"classic"}),t.addEventListener("activated",e=>{(e.isUpdate||e.isExternal)&&window.location.reload()}),t.addEventListener("installed",e=>{e.isUpdate||i==null||i()}),t.register({immediate:d}).then(e=>{s?s("/sw.js",e):a==null||a(e)}).catch(e=>{r==null||r(e)})}}return o=p(),l}export{v as registerSW};
-function __vite__mapDeps(indexes) {
-  if (!__vite__mapDeps.viteFileDeps) {
-    __vite__mapDeps.viteFileDeps = []
-  }
-  return indexes.map((i) => __vite__mapDeps.viteFileDeps[i])
-}

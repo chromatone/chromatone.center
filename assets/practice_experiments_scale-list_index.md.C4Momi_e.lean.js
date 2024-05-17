7bf883e591d2fe0800ab08c6df9f77c58fc041d7
davay42 <davay42@users.noreply.github.com>
2024-05-17 08:47:11 +0000

    deploy: ecff6823dfd125be4df3233debe9eed086a32cdc

---
+++ assets/practice_experiments_scale-list_index.md.C4Momi_e.lean.js
@@ -0,0 +1,5 @@
+import{ap as a,ac as r,O as n}from"./chunks/theme.GEXdnO73.js";import{F as o,G as l,I as c,C as p,E as d}from"./chunks/framework.BbXr-hJc.js";const f={class:"m-8 p-4 text-xs h-100"},h=JSON.parse('{"title":"Scale lists","description":"Lists of all scales from different sources","frontmatter":{"title":"Scale lists","description":"Lists of all scales from different sources","date":"2004-04-30T00:00:00.000Z"},"headers":[],"relativePath":"practice/experiments/scale-list/index.md","filePath":"practice/experiments/scale-list/index.md","lastUpdated":1715935440000}'),m={name:"practice/experiments/scale-list/index.md"},v=Object.assign(m,{setup(_){const t=[];for(let s in a){const e=a[s],i=r.intervals(e.chroma);t.push([i,e.title,Object.values(e.names).flat()])}return(s,e)=>(d(),o("div",null,[l("textarea",f,`
+`+c(p(n).all())+`
+`,1),l("textarea",{class:"m-8 p-4 text-xs h-100"},`
+`+c(t)+`
+`)]))}});export{h as __pageData,v as default};

e535681a7b1c485995d00ae61aab46e6a2c58a5b
davay <davay@defucc.me>
2024-05-29 15:32:15 +0700

    better matter

--- content/practice/experiments/matter/useMatter.js
+++ content/practice/experiments/matter/useMatter.js
@@ -2,22 +2,17 @@ import { ref, reactive, watch, onMounted, onBeforeUnmount } from 'vue';
 import Matter, { Render, Body, Bodies, World, Runner, Events, Engine, Query, MouseConstraint, Composites, Common, Vector } from 'matter-js';
 import { useResizeObserver } from '@vueuse/core';
 import MatterWrap from 'matter-wrap';
-import { midi } from '#/use/midi';
-import { synthOnce } from '#/use';
+import { midi, playKey } from '#/use/midi';
 import { Note } from 'tonal';
 
 Matter.use(MatterWrap);
-
-export const settings = reactive({
-  friction: 0.008,
-  bounce: 0.98,
-});
 
 let engine;
 let renderer;
 
 const canvas = ref(null);
 const initiated = ref(false);
+const running = ref(false)
 const box = reactive({ w: 100, h: 100 })
 
 export function useMatter() {
@@ -25,11 +20,8 @@ export function useMatter() {
   const setupMatterJs = () => {
     resizeBox()
     useResizeObserver(canvas, resizeBox)
-
     engine = Engine.create();
-    engine.gravity.scale = 0.0001;
-    engine.gravity.x = 0;
-    engine.gravity.y = 1;
+    engine.gravity.scale = 0;
 
     renderer = Render.create({
       element: canvas.value,
@@ -45,72 +37,6 @@ export function useMatter() {
 
     Runner.run(engine);
     Render.run(renderer);
-
-    const createShape = (x, y, note = Math.floor(Common.random(50, 100))) => {
-      const fillStyle = `hsl(${((note + 3) % 12) * 30}deg, 50%, 50%)`;
-      const circle = Bodies.circle(x, y, 25, {
-        frictionAir: settings.friction * Math.random(),
-        restitution: settings.bounce,
-        render: { fillStyle },
-        plugin: {
-          wrap: {
-            min: { x: 0, y: 0 },
-            max: { x: box.w, y: box.h },
-          },
-        },
-      });
-      circle.data = { note };
-      return circle;
-    };
-
-    const mouseControl = MouseConstraint.create(engine, {
-      element: canvas.value,
-      constraint: {
-        render: { visible: false },
-      },
-    });
-
-    const circles = Composites.stack();
-
-    World.add(engine.world, [mouseControl]);
-
-    Events.on(mouseControl, 'mousemove', ({ mouse }) => {
-      const hoveredShapes = Query.point(circles.bodies, mouse.position);
-      hoveredShapes.forEach((shape) => { shape.scale = 1.1; });
-    });
-
-    Events.on(mouseControl, 'mousedown', ({ mouse }) => {
-      const shape = createShape(mouse.position.x, mouse.position.y);
-      circles.bodies.push(shape);
-      World.add(engine.world, shape);
-    });
-
-    watch(() => midi.note, (note) => {
-      if (!note.velocity) return;
-      const shape = createShape(box.w * Math.random(), box.h * Math.random(), note.number);
-      Body.setVelocity(shape, Vector.create(Common.random(-10, 10), Common.random(-10, 10)));
-      circles.bodies.push(shape);
-      World.add(engine.world, shape);
-    });
-
-    Events.on(engine, 'collisionStart', (event) => {
-      for (const pair of event.pairs) {
-        for (let body of ['bodyA', 'bodyB']) {
-          if (!pair[body]?.data) continue
-          const hitBody = pair[body]
-          const originalStyle = hitBody.render.fillStyle;
-          if (originalStyle == 'black') continue;
-
-          hitBody.render.fillStyle = 'black';
-          synthOnce(Note.fromMidi(hitBody.data?.note));
-          setTimeout(() => {
-            hitBody.render.fillStyle = originalStyle || 'black';
-          }, 10);
-        }
-
-      }
-    });
-
   };
 
   const clearMatterJs = () => {
@@ -125,7 +51,8 @@ export function useMatter() {
     if (initiated.value) return;
     initiated.value = true;
     setupMatterJs();
-    useCenter()
+    const { center } = useCenter()
+    const { circles } = useCircles()
   });
 
   onBeforeUnmount(() => {
@@ -159,13 +86,102 @@ function resizeBox() {
   });
 }
 
+/* Circles */
+
+function useCircles() {
+  const mouseControl = MouseConstraint.create(engine, {
+    element: canvas.value,
+    constraint: {
+      render: { visible: false },
+    },
+  });
+
+  World.add(engine.world, [mouseControl]);
+
+  const circles = Composites.stack();
+
+  Events.on(engine, 'afterUpdate', () => {
+    circles.bodies.forEach(circle => {
+      const forceX = box.w / 2 - circle.position.x;
+      const forceY = box.h / 2 - circle.position.y;
+
+      // Apply a small force towards the center
+      const strength = 0.01; // Adjust this value to control the pull strength
+      Body.applyForce(circle, circle.position, { x: forceX * strength, y: forceY * strength });
+    })
+  })
+
+  function createShape(x, y, note = Math.floor(Common.random(50, 100))) {
+    const strokeStyle = `hsl(${((note + 3) % 12) * 30}deg, ${(note + 3)}%, 50%)`;
+
+    const circle = Bodies.circle(x, y, 120 - note, {
+      frictionAir: 0.0000008,
+      density: 10,
+      restitution: 0.98,
+      render: {
+        lineWidth: 2,
+        strokeStyle,
+        fillStyle: 'transparent'
+      },
+      plugin: {
+        wrap: {
+          min: { x: 0, y: 0 },
+          max: { x: box.w, y: box.h },
+        },
+      },
+    });
+    circle.data = { note };
+    return circle;
+  };
+
+  Events.on(mouseControl, 'mousemove', ({ mouse }) => {
+    const hoveredShapes = Query.point(circles.bodies, mouse.position);
+    hoveredShapes.forEach((shape) => { shape.scale = 1.1; });
+  });
+
+  Events.on(mouseControl, 'mousedown', ({ mouse }) => {
+    const hoveredShapes = Query.point(circles.bodies, mouse.position);
+    if (hoveredShapes.length > 0) return
+    const shape = createShape(mouse.position.x, mouse.position.y);
+    circles.bodies.push(shape);
+    World.add(engine.world, shape);
+  });
+
+  Events.on(engine, 'collisionStart', (event) => {
+    for (const pair of event.pairs) {
+      for (let body of ['bodyA', 'bodyB']) {
+        if (!pair[body]?.data) continue
+        const hitBody = pair[body]
+        const originalStyle = hitBody.render.strokeStyle;
+        if (hitBody.render.fillStyle != 'transparent') continue;
+
+        hitBody.render.fillStyle = originalStyle;
+        const note = Note.fromMidi(hitBody.data?.note)
+
+        playKey(note.slice(0, -1), parseInt(note.slice(-1)) - 4, false, 1, 0.5)
+
+        setTimeout(() => {
+          hitBody.render.fillStyle = 'transparent'
+          playKey(note.slice(0, -1), parseInt(note.slice(-1)) - 4, true)
+        }, 30);
+      }
+
+    }
+  });
+
+  return {
+    circles
+  }
+}
+
 
 /* CENTER SHAPE */
 
 function useCenter() {
-  const running = ref(true)
+
   const center = Bodies.polygon(box.w / 2, box.h / 2, 3, Math.min(box.w / 4, box.h / 4), {
     isStatic: true,
+    restitution: 100,
     render: {
       lineWidth: 0,
       strokeStyle: 'yellow',
@@ -180,14 +196,13 @@ function useCenter() {
     Body.setPosition(center, { x: w / 2, y: h / 2 });
   })
 
+
   let time = 0;
-  const updateGravity = () => {
+  Events.on(engine, 'afterUpdate', () => {
     time += 0.001;
     Body.rotate(center, 0.01);
-    if (!running.value) return
-    requestAnimationFrame(updateGravity);
-  };
-  updateGravity();
+  })
+
 
   return {
     running,

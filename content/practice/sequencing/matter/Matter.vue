c031e02ea86b14ac5d4fb419722570c491fe2f49
davay <davay@defucc.me>
2024-06-27 11:22:07 +0700

    center joystick

--- content/practice/sequencing/matter/Matter.vue
+++ content/practice/sequencing/matter/Matter.vue
@@ -19,13 +19,14 @@ function move(ev) {
 .flex.flex-col.gap-2.absolute.top-0.bottom-0.right-0.left-0.flex.items-center.justify-center.pointer-events-none(v-if="!running")  
   .text-2xl CLICK ANYWHERE
   .text-lg Use arrow keys to control the triangle
-Joystick.fixed.bottom-4.right-4.op-50(
-  :size="80"
-  base-color="#333e"
-  stick-color="#eeee"
-  :throttle="100"
-  @move="move"
-  @stop="stop"
-  )
+.flex.items-center.justify-center.fixed.bottom-8.w-full
+  Joystick.op-50(
+    :size="80"
+    base-color="#333e"
+    stick-color="#eeee"
+    :throttle="100"
+    @move="move"
+    @stop="stop"
+    )
 section.w-full.h-100svh(ref="canvas")
 </template>

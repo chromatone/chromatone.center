dea1905f838d10471cf105329fae0ab6e35e545a
davay42 <davay42@users.noreply.github.com>
2024-08-20 14:31:01 +0000

    deploy: bbc6e79789c5fcda981ae8bae9987b93a02e814b

--- assets/chunks/ControlStart.C8xG_wxN.js
+++
@@ -1,1 +0,0 @@
-import{x as e,y as o,J as n,ac as s,q as a}from"./framework.Bi_5INRF.js";const c={class:"button"};function r(t,d){return e(),o("button",c,[n(t.$slots,"default",{},()=>[s("Start")],!0)])}const _={},u=a(_,[["render",r],["__scopeId","data-v-fb351d24"]]);export{u as _};

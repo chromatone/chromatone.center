e9bb6ab529927de9d1ae12dfb3274b2800e0b7be
davay42 <davay42@users.noreply.github.com>
2024-09-05 10:14:26 +0000

    deploy: e03d26d5c3f53730ccd18a6d862f631e92ff7bf3

---
+++ assets/theory_notes_staff_index.md.BlPo64au.lean.js
@@ -0,0 +1,50 @@
+import{aD as d}from"./chunks/theme.T-vkwoWg.js";import{_ as b}from"./chunks/AbcRender.DScjML24.js";import{s}from"./chunks/state.DyWH8wRP.js";import{g as G,h as t,p,l as i,Q as u,au as o,R as a,f as h}from"./chunks/framework.BX1Y-ISu.js";import"./chunks/SaveSvg.GPKaLW3f.js";const m="/assets/kvintcirklen.BMYKVXGG.png",c="/assets/chromatic-c.YgNcEvSM.jpg",f="/assets/chromatic-Eb.xsmrZ7Xp.jpg",x="/assets/note-values-and-rests.CQymK56H.png",g="/assets/Bachlut1.BDcRdeuX.png",F=JSON.parse('{"title":"Classic European music staff notation","description":"How the sheet notes are written and read from classic era till today","frontmatter":{"title":"Classic European music staff notation","description":"How the sheet notes are written and read from classic era till today","date":"2021-08-12T00:00:00.000Z","cover":"kvintcirklen.png","links":["https://en.wikibooks.org/wiki/Music_Theory/Music_Notation_Systems","https://www.vexflow.com/"]},"headers":[],"relativePath":"theory/notes/staff/index.md","filePath":"theory/notes/staff/index.md","lastUpdated":1725531069000}'),A={name:"theory/notes/staff/index.md"},E=Object.assign(A,{setup(B){const r=`
+X:409
+T:Minuet in G Major
+T:BWV Anhang 114
+C:Christian Petzold (1677-1733)
+C:Formerly attributed to J. S. Bach
+C:(Guitar chords added)
+Z:ClassicMan at musescore.com
+Z:abc-edited-by: AW
+L:1/4
+M:3/4
+I:linebreak $
+K:G
+V:1 treble %nm="Piano"
+V:2 treble
+V:3 bass
+V:4 bass
+V:1
+"G"d G/A/"D7/a"B/c/ | "G/b"d .G .G | "C"e Mc/d/e/f/ | "G/b"g .G .G | "Am"Mc d/c/""B/A/ | "G"B c/B/A/G/ | "D"F "G"G/A/B/G/ |"D    D7"{B} A3 |$
+"G"d G/A/"D7/a"B/c/ | "G/b"d .G .G | "C"e Mc/d/e/f/ | "G/b"g .G .G | "Am"Mc d/c/"D/f#"B/A/ | "G"B c/B/"G/b"A/G/ | "Am/c"A "G/d"B/A/"D"G/F/ | "G"G3 ::$
+%
+"G"b g/a/b/g/ | "D/f#"a d/e/f/d/ | "Em"g e/f/g/d/ | "A"^c B/c/ A | "A"A/B/^c/d/e/f/ | "G/b"g "D"f "A/c#"e | "D"f "D/f#"A "A"^c | "D       D7"d3 |$
+"G/b"d G/F/ G | "C"e G/F/ G | "G/b"d "D/a"c "G"B | "D"A/G/F/G/ A | "D"D/E/F/G/A/B/ | "C/e"c "G"PB "D/f#"A | "G"B/d/ "G/b"G "D"F | "G"G3 :|
+V:2
+x3 | x3 | x3 | x3 | x3 | x3 | x3 | x3 |$ x3 | x3 | x3 | x3 | x3 | x3 | x3 | x3 ::$ x3 | x3 | x3 |
+x3 | x3 | x3 | x3 | x3 |$ x3 | x3 | x3 | x3 | x3 | x3 | x3 | [B,D]3 :|
+V:3
+[B,D]2 A, | B,3 | C3 | B,3 | A,3 | G,3 | D B, G, | D D,/C/B,/A,/ |$ B,2 A, | G, .B, .G, | C3 |
+B, C/B,/A,/G,/ | A,2 F, | G,2 B, | C D D, | G,2 G,, ::$ G,3 | F,3 | E, G, E, | A,2 A,, | A,3 |
+B, D ^C | D F, A, | D D, =C |$ z D2 | z E2 | B, A, G, | D2 z | z z F, | E, G, F, | G, B,, D, |
+G, D, G,, :|
+V:4
+G,2 x | x3 | x3 | x3 | x3 | x3 | x3 | x3 |$ x3 | x3 | x3 | x3 | x3 | x3 | x3 | x3 ::$ x3 | x3 |
+x3 | x3 | x3 | x3 | x3 | x3 |$ B,2 B, | C2 C | x3 | x3 | D,3 | x3 | x3 | x3 :|
+`;return(D,e)=>{const n=b,l=d;return h(),G("div",null,[t("p",null,[t("button",{style:p({background:i(s).colorize?"linear-gradient(#e66465, #9198e5)":""}),class:"button fixed right-16 bottom-4 z-20000 p-2 bg-light-400 dark-bg-dark-400 rounded-xl shadow active_bg-red-100",onClick:e[0]||(e[0]=z=>i(s).colorize=!i(s).colorize)},"Colorize notes",4)]),e[8]||(e[8]=u('<p><img src="'+m+'" alt=""></p><p>Standard notation is used to demonstrate how a piece is played. Unlike tablature, it applies to any instrument. It indicates key signatures, time signatures, rhythms, tempo, dynamics (how loud each instrument should be), and so on. A highly trained musician can sometimes take a piece of sheet music written in standard notation, look it over once or twice, and then play the song as though he or she had been playing it his or her whole life.</p><p>For instance, below is the C major scale, including a C at the end, in standard notation.</p><p>The standard notation staff has five lines and four spaces. From bottom to top the five lines are E G B D F, which is commonly memorized as an acrostic such as:</p><ul><li><strong>E</strong>very</li><li><strong>G</strong>ood</li><li><strong>B</strong>oy</li><li><strong>D</strong>oes</li><li><strong>F</strong>ine</li></ul><p>The four spaces between the five lines are F, A, C, and E, which should surely be easy for an English speaker to remember, because together they spell &quot;face&quot;.</p><p>But what about the first two notes, which are below the staff? Well, the second note is just below the E, so it must be D. The first is below that, so it must be C. It also has a line through it to indicate it is placed on an &quot;invisible&quot; line. This line is called a ledger line. A note could be placed below this ledger line, which would be B. Or a note could be placed below that, on another ledger line, and it would be A. Notes can continue to be placed on ledger lines above and below the staff infinitely, but extending too far from the staff is impractical, because the pitches will become very hard to read.</p><h2 id="clefs" tabindex="-1">Clefs <a class="header-anchor" href="#clefs" aria-label="Permalink to &quot;Clefs&quot;">​</a></h2>',8)),t("ol",null,[t("li",null,[e[1]||(e[1]=o("Treble (G) ")),a(n,{abc:`K:treble
+G8`})]),t("li",null,[e[2]||(e[2]=o("Bass (F) ")),a(n,{abc:`K:bass
+F,8`})]),t("li",null,[e[3]||(e[3]=o("Baritone (F) ")),a(n,{abc:`K:bass3
+F,8`})]),t("li",null,[e[4]||(e[4]=o("Tenor (C) ")),a(n,{abc:`K:tenor
+c,8`})]),t("li",null,[e[5]||(e[5]=o("Alto (C) ")),a(n,{abc:`K:alto
+c,8`})]),t("li",null,[e[6]||(e[6]=o("Mezzosoprano (C) ")),a(n,{abc:`K:alto2
+c,8`})]),t("li",null,[e[7]||(e[7]=o("Soprano (C) ")),a(n,{abc:`K:alto1
+c,8`})])]),e[9]||(e[9]=t("h2",{id:"note-pitch",tabindex:"-1"},[o("Note pitch "),t("a",{class:"header-anchor",href:"#note-pitch","aria-label":'Permalink to "Note pitch"'},"​")],-1)),e[10]||(e[10]=t("h3",{id:"natural-g",tabindex:"-1"},[o("Natural (G) "),t("a",{class:"header-anchor",href:"#natural-g","aria-label":'Permalink to "Natural (G)"'},"​")],-1)),a(n,{abc:"G8"}),a(n,{abc:`K:Gb
+=G8`}),e[11]||(e[11]=t("h3",{id:"sharp-g",tabindex:"-1"},[o("Sharp (G#) "),t("a",{class:"header-anchor",href:"#sharp-g","aria-label":'Permalink to "Sharp (G#)"'},"​")],-1)),a(n,{abc:"^G8"}),a(n,{abc:`K:Gb
+^^G8`}),e[12]||(e[12]=t("h3",{id:"flat-gb",tabindex:"-1"},[o("Flat (Gb) "),t("a",{class:"header-anchor",href:"#flat-gb","aria-label":'Permalink to "Flat (Gb)"'},"​")],-1)),a(n,{abc:"_G8"}),a(n,{abc:`K:C#
+__G8`}),e[13]||(e[13]=t("h3",{id:"ascending",tabindex:"-1"},[o("Ascending "),t("a",{class:"header-anchor",href:"#ascending","aria-label":'Permalink to "Ascending"'},"​")],-1)),e[14]||(e[14]=t("p",null,"A A# B C C# D D# E F F# G G# A",-1)),a(n,{responsive:"",abc:"A,^A,B,C^CD^DEF^FG^GA"}),e[15]||(e[15]=t("h3",{id:"descending",tabindex:"-1"},[o("Descending "),t("a",{class:"header-anchor",href:"#descending","aria-label":'Permalink to "Descending"'},"​")],-1)),e[16]||(e[16]=t("p",null,"A Ab G Gb F E Eb D Db C B Bb A",-1)),a(n,{responsive:"",abc:"a,_a,G_GFE_ED_DCB,_B,A,"}),e[17]||(e[17]=t("p",null,[t("img",{src:c,alt:""}),t("img",{src:f,alt:""})],-1)),e[18]||(e[18]=t("h2",{id:"note-values-durations",tabindex:"-1"},[o("Note values (durations) "),t("a",{class:"header-anchor",href:"#note-values-durations","aria-label":'Permalink to "Note values (durations)"'},"​")],-1)),e[19]||(e[19]=t("p",null,"Whole note = 2 half notes = 4 quarter notes = 8 eighth notes = 16 sixteenth notes",-1)),a(n,{responsive:"",abc:`M:4/4
+|G8|G4A4|G2A2B2c2|GDGDGDGD|G/D/G/D/G/D/G/D/G/D/G/D/G/D/G/D/|`}),e[20]||(e[20]=t("h3",{id:"dotted-notes",tabindex:"-1"},[o("Dotted notes "),t("a",{class:"header-anchor",href:"#dotted-notes","aria-label":'Permalink to "Dotted notes"'},"​")],-1)),a(n,{responsive:"",abc:`M:4/4
+|(G12|G4)|G5G2|G3GG3G|G3/2G/2G3/2G/2G3/2G/2G3/2G/2|`}),e[21]||(e[21]=t("h3",{id:"triplets",tabindex:"-1"},[o("Triplets "),t("a",{class:"header-anchor",href:"#triplets","aria-label":'Permalink to "Triplets"'},"​")],-1)),a(n,{responsive:"",abc:`M:4/4
+|(3G4A4B4|(3G2A2B2 (3G2A2B2| (3GAB (3GAB (3GAB (3GAB|`}),e[22]||(e[22]=t("h3",{id:"other-tuplets",tabindex:"-1"},[o("Other tuplets "),t("a",{class:"header-anchor",href:"#other-tuplets","aria-label":'Permalink to "Other tuplets"'},"​")],-1)),a(n,{responsive:"",abc:`M:4/4
+|(5G2A2B2c2d2|(7CDEFGAB|`}),e[23]||(e[23]=t("h3",{id:"rests",tabindex:"-1"},[o("Rests "),t("a",{class:"header-anchor",href:"#rests","aria-label":'Permalink to "Rests"'},"​")],-1)),a(n,{responsive:"",abc:`M:4/4
+|z8|z4z4|z2z2z2z2|zzzzzzzz|z/z/z/z/z/z/z/z/z/z/z/z/z/z/z/z/|`}),e[24]||(e[24]=t("p",null,[t("img",{src:x,alt:""})],-1)),e[25]||(e[25]=t("blockquote",null,[t("p",null,[t("img",{src:g,alt:""}),o(" J.S.Bach Prelude")])],-1)),e[26]||(e[26]=t("h3",{id:"alexander-scriabin-piano-concerto-in-f-sharp-minor-op-20",tabindex:"-1"},[o("Alexander Scriabin - Piano Concerto in F sharp minor, Op. 20 "),t("a",{class:"header-anchor",href:"#alexander-scriabin-piano-concerto-in-f-sharp-minor-op-20","aria-label":'Permalink to "Alexander Scriabin - Piano Concerto in F sharp minor, Op. 20"'},"​")],-1)),a(l,{video:"F734PyD3NAw"}),e[27]||(e[27]=t("h2",{id:"vector-render-of-staff-notation-by-abcjs",tabindex:"-1"},[o("Vector render of staff notation by ABCjs "),t("a",{class:"header-anchor",href:"#vector-render-of-staff-notation-by-abcjs","aria-label":'Permalink to "Vector render of staff notation by ABCjs"'},"​")],-1)),e[28]||(e[28]=t("p",null,"Note colorization is very useful to build connections between classic and Chromatone music theory visualizations.",-1)),a(n,{responsive:"",abc:r}),e[29]||(e[29]=t("p",null,[t("a",{href:"./../computer/abc/"},"Play with the ABC notations editor")],-1)),e[30]||(e[30]=t("p",null,[t("a",{href:"./sight-reading/"},"Sight reading")],-1)),e[31]||(e[31]=t("p",null,[t("a",{href:"./evolution/"},"European tradition")],-1))])}}});export{F as __pageData,E as default};

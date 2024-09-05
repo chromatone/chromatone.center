8bd6dd5fe49649cf9afff116fee82e74159dfe57
davay42 <davay42@users.noreply.github.com>
2024-09-05 07:11:15 +0000

    deploy: bd04d5aec36582ee76c201191612198cf480da69

---
+++ assets/chunks/ControlStart.Cj4AxoTO.js
@@ -0,0 +1,1 @@
+import{f as o,g as n,x as s,au as r,_ as a}from"./framework.BX1Y-ISu.js";const _={class:"button"};function c(e,t){return o(),n("button",_,[s(e.$slots,"default",{},()=>[t[0]||(t[0]=r("Start"))],!0)])}const d={},l=a(d,[["render",c],["__scopeId","data-v-fb351d24"]]);export{l as _};

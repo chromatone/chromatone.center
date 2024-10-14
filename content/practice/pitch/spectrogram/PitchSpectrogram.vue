efa6e7ef93d02f0e0f0cff0018dd35f058b88774
davay <davay@defucc.me>
2024-10-14 15:33:53 +0700

    feat: better spectrogram controls

--- content/practice/pitch/spectrogram/PitchSpectrogram.vue
+++ content/practice/pitch/spectrogram/PitchSpectrogram.vue
@@ -1,8 +1,7 @@
 <script setup>
 import { initGetUserMedia, master } from '#/use/audio'
-import { freqPitch } from '#/use/calculations'
 import { useMicrophone } from '#/use/mic'
-import { onKeyStroke, useStorage, useWindowSize } from '@vueuse/core'
+import { onKeyStroke, useCycleList, useStorage, useWindowSize } from '@vueuse/core'
 import { ref, computed, reactive, onMounted, watch, onUnmounted } from 'vue'
 import { useClamp } from '@vueuse/math'
 
@@ -10,27 +9,17 @@ const canvasElement = ref()
 const video = ref()
 
 const { mic, input } = useMicrophone()
+const { width, height } = useWindowSize()
 
 const paused = ref(false)
 
-onKeyStroke(' ', (e) => {
-  paused.value = !paused.value
-  e.preventDefault()
-})
 
-onKeyStroke('Enter', (e) => {
-  clear()
-  e.preventDefault()
-})
 
 let canvas, ctx, tempCanvas, tempCtx
+let audio
 
-function clear() {
-  ctx.fillStyle = '#000'
-  ctx.fillRect(0, 0, state.width, state.height)
-}
 
-const { width, height } = useWindowSize()
+
 
 const state = reactive({
   initiated: false,
@@ -48,44 +37,71 @@ function dragScreen(drag) {
   state.speedCount -= drag.delta[0] / 2
 }
 
-watch([width, height], ([newWidth, newHeight]) => {
+watch([width, height], ([w, h]) => {
   if (canvas && tempCanvas) {
-    canvas.width = newWidth
-    canvas.height = newHeight
-    tempCanvas.width = newWidth
-    tempCanvas.height = newHeight
+    canvas.width = tempCanvas.width = w
+    canvas.height = tempCanvas.height = h
+    clear()
   }
 })
 
 onMounted(() => {
   initGetUserMedia()
-  canvas = document.getElementById('spectrogram')
+  canvas = canvasElement.value
   ctx = canvas.getContext('2d')
   tempCanvas = document.createElement('canvas')
   tempCtx = tempCanvas.getContext('2d')
   tempCanvas.width = state.width
   tempCanvas.height = state.height
-  ctx.fillStyle = '#000'
-  ctx.fillRect(0, 0, state.width, state.height)
-
-  const stream = canvas.captureStream();
-
+  clear()
+  const stream = canvasElement.value.captureStream();
   video.value.srcObject = stream;
-
   video.value.play()
     .then(() => video.value?.requestPictureInPicture?.())
     .catch(error => console.error(error));
 });
 
-// onUnmounted(() => {
-//   audio.stop()
-// })
+const freqPitch = (freq) => 12 * (Math.log(Number(freq) / 440) / Math.log(2))
 
-function increaseContrast(value, steepness = 10, midpoint = 0.5) {
-  return 1 / (1 + Math.exp(-steepness * (value - midpoint)));
+const colorIt = (freq, value) => `hsl(${freqPitch(freq) * 30}, ${value * 100}%, ${value * 75}%)`
+
+const steepness = useClamp(10, 3, 30)
+const midpoint = useClamp(0.5, 0, 1)
+const sigmoid = (value) => 1 / (1 + Math.exp(-steepness.value * (value - midpoint.value)))
+
+function clear() {
+  ctx.fillStyle = '#000';
+  ctx.fillRect(0, 0, state.width, state.height)
 }
 
-let audio
+const onCanvasDraw = (instance) => {
+  if (paused.value) return
+  tempCtx.drawImage(canvas, 0, 0, state.width, state.height)
+  let bars = instance.getBars()
+  for (let i = 0; i < bars.length; i++) {
+    const centerFreq = (bars[i].freqLo + bars[i].freqHi) / 2
+    ctx.fillStyle = colorIt(centerFreq, sigmoid(bars[i].value[0]))
+
+    if (state.vertical) {
+      ctx.fillRect(i * (state.width / bars.length), 0, state.width / bars.length, state.speed)
+    } else {
+      ctx.fillRect(state.width - state.speed, state.height - (i + 1) * (state.height / bars.length), state.speed, state.height / bars.length)
+    }
+  }
+  if (state.vertical) {
+    ctx.translate(0, state.speed)
+  } else {
+    ctx.translate(-state.speed, 0)
+  }
+  ctx.drawImage(tempCanvas, 0, 0, state.width, state.height)
+  ctx.setTransform(1, 0, 0, 1, 0, 0);
+}
+
+const fftSize = useCycleList([4096, 8192, 16384, 32768], { initialValue: 8192 })
+watch(fftSize.state, size => audio && (audio.fftSize = size))
+
+const smoothing = useClamp(0.5, 0, 0.9)
+watch(smoothing, s => audio && (audio.smoothing = s))
 
 function initiate() {
   navigator.mediaDevices.getUserMedia({ audio: true, video: false }).then(async () => {
@@ -95,77 +111,60 @@ function initiate() {
       mode: 1,
       connectSpeakers: false,
       volume: 0,
+      fftSize: fftSize.state.value,
+      smoothing: smoothing.value,
       useCanvas: false,
+      onCanvasDraw,
       source: master.meter,
-      onCanvasDraw(instance) {
-        if (paused.value) return
-        tempCtx.drawImage(canvas, 0, 0, state.width, state.height)
-        let bars = instance.getBars()
-        for (let i = 0; i < bars.length; i++) {
-
-          ctx.fillStyle = colorIt((bars[i].freqLo + bars[i].freqHi) / 2, increaseContrast(bars[i].value[0]))
-
-          if (state.vertical) {
-            ctx.fillRect(i * (state.width / bars.length), 0, state.width / bars.length, state.speed)
-          } else {
-            ctx.fillRect(state.width - state.speed, state.height - (i + 1) * (state.height / bars.length), state.speed, state.height / bars.length)
-          }
-        }
-        if (state.vertical) {
-          ctx.translate(0, state.speed)
-        } else {
-          ctx.translate(-state.speed, 0)
-        }
-        ctx.drawImage(tempCanvas, 0, 0, state.width, state.height)
-        ctx.setTransform(1, 0, 0, 1, 0, 0);
-      }
     })
     state.initiated = true
     mic.open = true
     audio.connectInput(input)
-
-
   }).catch((e) => {
     console.log('mic denied', e)
   })
 }
 
+onKeyStroke(' ', (e) => {
+  paused.value = !paused.value
+  e.preventDefault()
+})
 
-function colorIt(freq, value) {
-  let v = Math.pow(value + state.offset, state.pow)
-  return `hsl(${freqPitch(freq) * 30}, ${v * 100}%, ${v * 70}%)`
-}
+onKeyStroke('Enter', (e) => {
+  clear()
+  e.preventDefault()
+})
+
+
 
 </script>
 
 <template lang="pug">
 .flex.flex-col.justify-center
-
   .fullscreen-container.text-white#screen
-
     canvas#spectrogram.cursor-pointer.max-w-full(
-
       ref="canvasElement"
       v-drag="dragScreen"
       :width="state.width"
       :height="state.height"
-    )
+      )
     control-start.absolute(
       v-if="!state.initiated" 
       @click="initiate()")
-    .absolute.top-4.left-4.text-xl.select-none x{{ state.speed }}
-    button.absolute.bottom-4.left-4.text-xl.select-none.cursor-pointer(@mousedown="paused = !paused")
+    .absolute.top-4.left-4.text-xl.select-none.cursor-pointer(@pointerdown="fftSize.next()") x{{ state.speed }} {{ fftSize.state }}
+    button.absolute.bottom-4.left-4.text-xl.select-none.cursor-pointer(@pointerdown="paused = !paused")
       .i-la-play(v-if="paused")
       .i-la-pause(v-else)
-    button.absolute.bottom-4.right-4.text-xl.select-none.cursor-pointer(@mousedown="state.vertical = !state.vertical")
+    button.absolute.bottom-4.right-4.text-xl.select-none.cursor-pointer(@pointerdown="state.vertical = !state.vertical")
       .i-la-arrow-left(v-if="!state.vertical")
       .i-la-arrow-down(v-else)
-    button.absolute.top-4.right-4.text-xl.select-none.cursor-pointer(@mousedown="clear()")
+    button.absolute.top-4.right-4.text-xl.select-none.cursor-pointer(@pointerdown="clear()")
       .i-la-trash-alt
-  .absolute.w-full.bottom-2.flex.items-center.gap-2.px-2.flex-wrap 
-    .is-group.flex.flex-wrap.gap-2.flex-0.p-2.op-20.hover-op-80.transition
-      ControlRotary(v-model="state.pow" :min="1" :max="10" :step="1" :fixed="0" param="POW")
-      ControlRotary(v-model="state.offset" :min="-.5" :max=".5" :step=".0001" param="OFFSET" :fixed="2")
+  .absolute.w-full.bottom-2.flex.items-end.gap-2.px-2.flex-wrap()
+    .is-group.flex.flex-wrap.gap-2.flex-0.p-2.op-20.hover-op-80.transition.ml-16(v-if="state.initiated")
+      ControlRotary(v-model="steepness" :min="3" :max="30" :step="0.0001" :fixed="2" param="STEEP")
+      ControlRotary(v-model="midpoint" :min="0" :max="1" :step=".0001" param="MID" :fixed="2")
+      ControlRotary(v-model="smoothing" :min="0" :max="1" :step=".0001" param="SMTH" :fixed="2")
     .flex-1
     .scale-75.max-h-60.overflow-clip.relative.text-white.op-20.hover-op-80.transition 
       .absolute.p-2.opacity-70.touch-none.select-none.text-md Right click here to enter Picture-In-Picture mode

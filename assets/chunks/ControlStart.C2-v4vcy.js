e487ccb4ba1fb1ee4a06d05f6734cdfa2af27b7f
davay42 <davay42@users.noreply.github.com>
2024-10-14 16:31:52 +0000

    deploy: 77770940e2849904303da2146a2028c66db865fb

--- assets/chunks/ControlStart.C2-v4vcy.js
+++
@@ -1,1 +0,0 @@
-import{x as o,y as n,J as s,au as r,q as a}from"./framework.DzzWumix.js";const _={class:"button"};function c(e,t){return o(),n("button",_,[s(e.$slots,"default",{},()=>[t[0]||(t[0]=r("Start"))],!0)])}const d={},l=a(d,[["render",c],["__scopeId","data-v-fb351d24"]]);export{l as _};

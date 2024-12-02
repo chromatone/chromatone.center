4ba2e6544e9b08c8e65f3a0541d49255211d8c63
davay <davay@defucc.me>
2024-12-03 00:04:55 +0700

    feat: better db

--- content/practice/external/tools.data.js
+++ content/practice/external/tools.data.js
@@ -1,15 +1,15 @@
-import { useItems } from "../../database"
+import { createDirectus, rest, staticToken, readItems } from '@directus/sdk'
 
 export default {
-  async load() {
-    const tools = await useItems('tools', {
+  load: async () => await createDirectus('https://db.chromatone.center/')
+    .with(rest())
+    .with(staticToken('W12HVtPiXYK3V5baFBO28I0XwgkGH9YY'))
+    .request(readItems('tools', {
       sort: ['title'],
       filter: {
         type: {
           _eq: 'tool'
         }
       }
-    })
-    return tools || []
-  }
+    }))
 }

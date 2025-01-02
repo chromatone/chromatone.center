384f84754e52146062db0558d95740f96a0ba0bf
davay <davay@defucc.me>
2025-01-02 19:02:39 +0700

    feat: polyrhythm experiment

---
+++ content/practice/experiments/polyrhythms/PolyRhythms-9.vue
@@ -0,0 +1,28 @@
+<script setup>
+import { useTempo } from '#/use';
+
+const tempo = useTempo()
+
+const getRotation = (ticks = 0, ratio = 1) => ((ticks / (192 * ratio)) % 1) * 360;
+
+const ratios = [9, 8, 7, 6, 5, 4, 3, 2, 1]
+</script>
+
+<template lang='pug'>
+svg(version="1.1" baseProfile="full" :viewBox="`0 0 500 500`" xmlns="http://www.w3.org/2000/svg")
+  g(v-for="ratio in ratios" :key="ratio"
+    :transform="`translate(250, 250) rotate(${getRotation(tempo.ticks, ratio)}) `")
+    circle(:r="20+ratio*20" fill="#3333" stroke="black" stroke-width="1")
+    g(
+      v-for="(_,mark) in ratio" :key="mark"
+      :transform="`rotate(${360* mark/ratio}) `")
+      line(
+        :y2="-20-ratio*20" 
+        :stroke-width="mark==0? 2:1"
+        :stroke="mark==0? 'white':'#aaa3'" 
+        )
+      circle(
+        :r="mark==0? 3:1" fill="#eee" :cy="-20-ratio*20+3" 
+        )
+  circle(r="3" fill="#aaa" :transform="`translate(250, 250) `")
+</template>
---
+++ content/practice/experiments/polyrhythms/index.md
@@ -0,0 +1,15 @@
+---
+title: Polyrhythmic circles
+description: Vistual prototype for a real life device
+date: 2015-01-02
+layout: app
+cover: poly.png
+---
+
+<script setup>
+import { defineClientComponent } from 'vitepress'
+
+const PolyRhythms = defineClientComponent(() => import('./PolyRhythms.vue'))
+</script>
+
+<PolyRhythms />
